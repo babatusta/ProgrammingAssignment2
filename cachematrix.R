42de4ed132a8059dff1cfbdec4282558fa00889f
babatusta <marko.lucic15@gmail.com>
2014-12-19 22:20:48 +0100

    first commit of changes - first solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+##R Programming Course on Coursera, Assignment 2
+#The goal is to make an empty "storage" for the inverse of a matrix, so it can be fetched quickly, 
+#without the need to compute the inverse again if the matrix remains unchanged.
 
-## Write a short comment describing this function
+#So, the first function creates the "storage" for the inverse of any (square and non-singular) matrix x 
+#in the environment different from global, enclosed in this function. It defines functions needed to: 
+#1. register any changes to the matrix, 2. retrieve the matrix, 3. compute the inverse and 4. retrieve the inverse.
+makeCacheMatrix<-function(x=matrix()) {
+      if (nrow(x)!=ncol(x)) stop ("x is not a square matrix")      
+      Inverse<-NULL
+      setMatrix<-function(y) {
+            x<<-y
+            Inverse<<-NULL
+      }
+      getMatrix<-function() x
+      setInverse<-function(solve) Inverse<<-solve(x)
+      getInverse<-function() Inverse
+      list(setMatrix=setMatrix,
+           getMatrix=getMatrix,
+           setInverse=setInverse,
+           getInverse=getInverse)
+}
 
-makeCacheMatrix <- function(x = matrix()) {
-
+#The second function calculates the inverse, but first it checks if the inverse, for the unchanged matrix, 
+#is "stored" by the first function.
+cacheSolve<-function(x,...) {
+      Inverse<-x$getInverse()
+      if(!is.null(Inverse)) {
+            message ("getting cached data")
+            return(Inverse)
+      }
+      data<-x$getMatrix()
+      Inverse<-solve(data)
+      x$setInverse(Inverse,...)
+      Inverse
 }
 
 
-## Write a short comment describing this function
+#Return a matrix that is the inverse of 'x'
+A<-matrix(rnorm(25),5,5)
+m<-makeCacheMatrix(A)
+B<-cacheSolve(m)
+B
+#B should be the inverse of 'x' (that is, A in my case)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+#additional:basic test
+round(A%*%B, digits=0)#Matrix multiplied by its inverse results in an identity matrix.
+#So, this should yield an identity matrix, with 1's on its diagonal, and otherwise zeros.
+#It's rounded because we expect very small numbers that are very close to zero.
